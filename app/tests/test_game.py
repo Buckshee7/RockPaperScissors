c69290c5cf2430fc774bd6e2c62219e8067a0b2d
Timothy Saudners <timsaunders88@gmail.com>
2020-08-01 17:14:20 +0100

    added test for generate_computer_player to make sure choices were being selected at correct rate

--- app/tests/test_game.py
+++ app/tests/test_game.py
@@ -1,12 +1,14 @@
 import unittest
 from app.modules.player import Player
-from app.modules.game import play_game
+from app.modules.game import play_game, generate_computer_player
 
 class TestGame(unittest.TestCase):
     def setUp(self):
         self.player_1 = Player("Tim", "Rock")
         self.player_2 = Player("Ally", "Paper")
         self.player_3 = Player("Jarrod", "Scissors")
+        self.player_4 = Player("Cheater", "Lava")
+
 
     def test_same_choice(self):
         self.assertEqual(None, play_game(self.player_1, self.player_1))
@@ -18,11 +20,25 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.player_1, play_game(self.player_1, self.player_3))
     
     def test_lava_scissors(self):
-        player_4 = Player("Cheater", "Lava")
-        self.assertEqual(player_4, play_game(player_4, self.player_3))
+        self.assertEqual(self.player_4, play_game(self.player_4, self.player_3))
 
     def test_rock_lava(self):
-        player_4 = Player("Cheater", "Lava")
-        self.assertEqual(player_4, play_game(self.player_1, player_4))
+        self.assertEqual(self.player_4, play_game(self.player_1, self.player_4))
 
-    
+    # Computer should pick rock/paper/scissors @ 30% and lava @ 10%
+    def test_generate_computer_player(self):
+        thousand_computers = []
+        for i in range(1000):
+            thousand_computers.append(generate_computer_player())
+        thousand_choices = []
+        for computer in thousand_computers:
+            thousand_choices.append(computer.choice)
+        rocks = thousand_choices.count("Rock")
+        papers = thousand_choices.count("Paper")
+        scissors = thousand_choices.count("Scissors")
+        lavas = thousand_choices.count("Lava")
+
+        self.assertAlmostEqual(300, rocks, delta = 30)
+        self.assertAlmostEqual(300, papers, delta = 30)
+        self.assertAlmostEqual(300, scissors, delta = 30)
+        self.assertAlmostEqual(100, lavas, delta = 10)
